9f87757a0d69091960b961e75e0352139a19e537
KarkiAdit <adityakarki728@gmail.com>
2024-09-17 14:45:10 -0500

    Day8 Qn6 optimized v.0.1

--- day8-qn6-easy.py
+++ day8-qn6-easy.py
@@ -19,3 +19,22 @@ class Solution:
                 if unique_char_map[char] < 0:
                     return False
         return True
+    
+# In O(1) space
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # If the lengths are not equal, it's not an anagram
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        # Increment counter for s
+        for char in s:
+            count[ord(char) - ord('a')] += 1
+        # Decrement counter for t
+        for char in t:
+            idx = ord(char) - ord('a')
+            count[idx] -= 1
+            if count[idx] < 0:
+                return False    
+        return True
+
