6a27f85566bff452f86268f0ad509eb92b947ada
KarkiAdit <adityakarki728@gmail.com>
2024-09-23 00:22:21 -0500

    Practice Day11 qn4 optimized v2.0

--- day11-qn4-medium.py
+++ day11-qn4-medium.py
@@ -18,6 +18,26 @@ class Solution:
                 p1 += 1
             window_size = max(window_size, p2 - p1 + 1)
         return window_size
+
+# Optmized Hash map solution
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        p1 = 0
+        # Track the index of all unique chars in the current window
+        window_unique_chars = {}
+        curr_max = 0
+        for p2 in range(len(s)):
+            char = s[p2]
+            # Move p1 one step to right of the repeating character
+            if char in window_unique_chars and window_unique_chars[char] >= p1:
+                p1  = window_unique_chars[char] + 1
+            # Update the current window
+            window_unique_chars[char] = p2
+            curr_max = max(curr_max, p2 - p1 + 1)
+        return curr_max
+
+
+
             
 
         
