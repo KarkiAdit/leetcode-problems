4d4a47e6cd68e3db3d5e2cb319ab55adb6269cf6
KarkiAdit <adityakarki728@gmail.com>
2024-09-23 00:51:55 -0500

    Practice Day12 Qn2 Optmized v2.0

--- day12-qn2-medium.py
+++ day12-qn2-medium.py
@@ -16,4 +16,22 @@ class Solution:
             anagram_list.append(processed_strs)
         return anagram_list
 
-        
+# Optimized O(m * n) solution
+
+class Solution:
+    def equiv_char_tuple(self, s: str) -> tuple:
+        # Create a frequency count for each character in the string
+        char_count = [0] * 26
+        for char in s:
+            char_count[ord(char) - 97] += 1
+        return tuple(char_count)
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grouped_anagram_map = {}
+        # Loop through the strings and group them by character frequency tuple
+        for s in strs:
+            s_tupled = self.equiv_char_tuple(s)
+            grouped_anagram_map[s_tupled].append(s)
+
+        return list(grouped_anagram_map.values())
+
